2bb0f7291dedb1913ad49c2fe30c5cd8b79ce3fb
Lijwy <lijwy@outlook.com>
2023-04-10 02:05:04 +0800

    update unique rm/ism

--- sqanti_summarize.sh
+++ sqanti_summarize.sh
@@ -2,13 +2,25 @@
 
 # Copy this script to the assembly that you want to analyze in 03.sqanti
 echo 'analysis for flair' > sqanti_summary.txt
+
 wc -l *flair.*classification.txt | awk '{print "transcripts assembled: " $1 -1}' >> sqanti_summary.txt # no. of transcripts (-1 for the header line)
+
 grep 'full-splice_match' *flair.*classification.txt | wc -l | awk '{print "FSM:\t" $1}' >> sqanti_summary.txt #for fsm
-grep 'full-splice_match' *flair.*classification.txt | cut -f8 | sort | uniq | wc -l | awk '{print "Unique FSM:\t" $1}' >> sqanti_summary.txt #for unique fsm
-grep 'full-splice_match' *flair.*classification.txt | grep -e 'reference_match' -e 'mono-exon' | wc -l | awk '{print "RM:\t" $1}' >> sqanti_summary.txt #for rm
-grep 'full-splice_match' *flair.*classification.txt | grep -e 'reference_match' -e 'mono-exon' | cut -f8 | sort | uniq | wc -l | awk '{print "Unique RM:\t" $1}' >> sqanti_summary.txt #for unique rm
+grep 'full-splice_match' *flair.*classification.txt | \
+cut -f8 | sort | uniq | wc -l | awk '{print "Unique FSM:\t" $1}' >> sqanti_summary.txt #for unique fsm
+
+grep 'full-splice_match' *flair.*classification.txt | grep -e 'reference_match' -e 'mono-exon' | \
+wc -l | awk '{print "RM:\t" $1}' >> sqanti_summary.txt #for rm
+grep 'full-splice_match' *flair.*classification.txt | grep -e 'reference_match' -e 'mono-exon' | \
+cut -f8 | sort | uniq | wc -l | awk '{print "Unique RM:\t" $1}' >> sqanti_summary.txt #for unique rm
+
 grep 'incomplete-splice_match' *flair.*classification.txt | wc -l | awk '{print "ISM:\t" $1}' >> sqanti_summary.txt #for ism
-grep 'incomplete-splice_match' *flair.*classification.txt | cut -f8 | sort | uniq | wc -l | awk '{print "Unique ISM:\t" $1}' >> sqanti_summary.txt #for unique ism
+grep 'incomplete-splice_match' *flair.*classification.txt | \
+cut -f8 | sort | uniq | wc -l | awk '{print "Unique ISM:\t" $1}' >> sqanti_summary.txt #for unique ism
+
+grep -e 'full-splice_match' -e 'incomplete-splice_match' *flair.*classification.txt | \
+cut -f8 | sort | uniq | wc -l | awk '{print "Unique RM/ISM:\t" $1}' >> sqanti_summary.txt #for unique rm/ism
+
 grep 'novel_in_catalog' *flair.*classification.txt | wc -l | awk '{print "NIC:\t" $1}' >> sqanti_summary.txt # for NIC
 grep 'novel_not_in_catalog' *flair.*classification.txt | wc -l | awk '{print "NNC:\t" $1}' >> sqanti_summary.txt # for NNC
 grep 'antisense' *flair.*classification.txt | wc -l | awk '{print "antisense:\t" $1}'>> sqanti_summary.txt # for antisense
@@ -16,14 +28,27 @@ grep 'genic' *flair.*classification.txt | wc -l | awk '{print "genic:\t" $1}' >>
 grep 'intergenic' *flair.*classification.txt | wc -l | awk '{print "intergenic:\t" $1}' >> sqanti_summary.txt # for intergenic
 grep 'fusion' *flair.*classification.txt | wc -l | awk '{print "fusion:\t" $1}' >> sqanti_summary.txt # for fusion
 
+
 echo 'analysis for stringtie.guided' >> sqanti_summary.txt
+
 wc -l *stringtie.guided.*classification.txt | awk '{print "transcripts assembled: " $1 -1}' >> sqanti_summary.txt # no. of transcripts (-1 for the header line)
+
 grep 'full-splice_match' *stringtie.guided.*classification.txt | wc -l | awk '{print "FSM:\t" $1}' >> sqanti_summary.txt #for fsm
-grep 'full-splice_match' *stringtie.guided.*classification.txt | cut -f8 | sort | uniq | wc -l | awk '{print "Unique FSM:\t" $1}' >> sqanti_summary.txt #for unique fsm
-grep 'full-splice_match' *stringtie.guided.*classification.txt | grep -e 'reference_match' -e 'mono-exon' | wc -l | awk '{print "RM:\t" $1}' >> sqanti_summary.txt #for rm
-grep 'full-splice_match' *stringtie.guided.*classification.txt | grep -e 'reference_match' -e 'mono-exon' | cut -f8 | sort | uniq | wc -l | awk '{print "Unique RM:\t" $1}' >> sqanti_summary.txt #for unique rm
+grep 'full-splice_match' *stringtie.guided.*classification.txt | \
+cut -f8 | sort | uniq | wc -l | awk '{print "Unique FSM:\t" $1}' >> sqanti_summary.txt #for unique fsm
+
+grep 'full-splice_match' *stringtie.guided.*classification.txt | grep -e 'reference_match' -e 'mono-exon' | \
+wc -l | awk '{print "RM:\t" $1}' >> sqanti_summary.txt #for rm
+grep 'full-splice_match' *stringtie.guided.*classification.txt | grep -e 'reference_match' -e 'mono-exon' | \
+cut -f8 | sort | uniq | wc -l | awk '{print "Unique RM:\t" $1}' >> sqanti_summary.txt #for unique rm
+
 grep 'incomplete-splice_match' *stringtie.guided.*classification.txt | wc -l | awk '{print "ISM:\t" $1}' >> sqanti_summary.txt #for ism
-grep 'incomplete-splice_match' *stringtie.guided.*classification.txt | cut -f8 | sort | uniq | wc -l | awk '{print "Unique ISM:\t" $1}' >> sqanti_summary.txt #for unique ism
+grep 'incomplete-splice_match' *stringtie.guided.*classification.txt | \
+cut -f8 | sort | uniq | wc -l | awk '{print "Unique ISM:\t" $1}' >> sqanti_summary.txt #for unique ism
+
+grep -e 'full-splice_match' -e 'incomplete-splice_match' *stringtie.guided.*classification.txt | \
+cut -f8 | sort | uniq | wc -l | awk '{print "Unique RM/ISM:\t" $1}' >> sqanti_summary.txt #for unique rm\ism
+
 grep 'novel_in_catalog' *stringtie.guided.*classification.txt | wc -l | awk '{print "NIC:\t" $1}' >> sqanti_summary.txt # for NIC
 grep 'novel_not_in_catalog' *stringtie.guided.*classification.txt | wc -l | awk '{print "NNC:\t" $1}' >> sqanti_summary.txt # for NNC
 grep 'antisense' *stringtie.guided.*classification.txt | wc -l | awk '{print "antisense:\t" $1}'>> sqanti_summary.txt # for antisense
@@ -31,14 +56,26 @@ grep 'genic' *stringtie.guided.*classification.txt | wc -l | awk '{print "genic:
 grep 'intergenic' *stringtie.guided.*classification.txt | wc -l | awk '{print "intergenic:\t" $1}' >> sqanti_summary.txt # for intergenic
 grep 'fusion' *stringtie.guided.*classification.txt | wc -l | awk '{print "fusion:\t" $1}' >> sqanti_summary.txt # for fusion
 
+
 echo 'analysis for stringtie.free' >> sqanti_summary.txt
+
 wc -l *stringtie.free.*classification.txt | awk '{print "transcripts assembled: " $1 -1}' >> sqanti_summary.txt # no. of transcripts (-1 for the header line)
 grep 'full-splice_match' *stringtie.free.*classification.txt | wc -l | awk '{print "FSM:\t" $1}' >> sqanti_summary.txt #for fsm
-grep 'full-splice_match' *stringtie.free.*classification.txt | cut -f8 | sort | uniq | wc -l | awk '{print "Unique FSM:\t" $1}' >> sqanti_summary.txt #for unique fsm
-grep 'full-splice_match' *stringtie.free.*classification.txt | grep -e 'reference_match' -e 'mono-exon' | wc -l | awk '{print "RM:\t" $1}' >> sqanti_summary.txt #for rm
-grep 'full-splice_match' *stringtie.free.*classification.txt | grep -e 'reference_match' -e 'mono-exon' | cut -f8 | sort | uniq | wc -l | awk '{print "Unique RM:\t" $1}' >> sqanti_summary.txt #for unique rm
+
+grep 'full-splice_match' *stringtie.free.*classification.txt | \
+cut -f8 | sort | uniq | wc -l | awk '{print "Unique FSM:\t" $1}' >> sqanti_summary.txt #for unique fsm
+grep 'full-splice_match' *stringtie.free.*classification.txt | grep -e 'reference_match' -e 'mono-exon' | \
+wc -l | awk '{print "RM:\t" $1}' >> sqanti_summary.txt #for rm
+grep 'full-splice_match' *stringtie.free.*classification.txt | grep -e 'reference_match' -e 'mono-exon' | \
+cut -f8 | sort | uniq | wc -l | awk '{print "Unique RM:\t" $1}' >> sqanti_summary.txt #for unique rm
+
 grep 'incomplete-splice_match' *stringtie.free.*classification.txt | wc -l | awk '{print "ISM:\t" $1}' >> sqanti_summary.txt #for ism
-grep 'incomplete-splice_match' *stringtie.free.*classification.txt | cut -f8 | sort | uniq | wc -l | awk '{print "Unique ISM:\t" $1}' >> sqanti_summary.txt #for unique ism
+grep 'incomplete-splice_match' *stringtie.free.*classification.txt | \
+cut -f8 | sort | uniq | wc -l | awk '{print "Unique ISM:\t" $1}' >> sqanti_summary.txt #for unique ism
+
+grep -e 'full-splice_match' -e 'incomplete-splice_match' *stringtie.free.*classification.txt | \
+cut -f8 | sort | uniq | wc -l | awk '{print "Unique RM/ISM:\t" $1}' >> sqanti_summary.txt #for unique rm/ism
+
 grep 'novel_in_catalog' *stringtie.free.*classification.txt | wc -l | awk '{print "NIC:\t" $1}' >> sqanti_summary.txt # for NIC
 grep 'novel_not_in_catalog' *stringtie.free.*classification.txt | wc -l | awk '{print "NNC:\t" $1}' >> sqanti_summary.txt # for NNC
 grep 'antisense' *stringtie.free.*classification.txt | wc -l | awk '{print "antisense:\t" $1}'>> sqanti_summary.txt # for antisense
@@ -46,14 +83,27 @@ grep 'genic' *stringtie.free.*classification.txt | wc -l | awk '{print "genic:\t
 grep 'intergenic' *stringtie.free.*classification.txt | wc -l | awk '{print "intergenic:\t" $1}' >> sqanti_summary.txt # for intergenic
 grep 'fusion' *stringtie.free.*classification.txt | wc -l | awk '{print "fusion:\t" $1}' >> sqanti_summary.txt # for fusion
 
+
 echo 'analysis for rb' >> sqanti_summary.txt
+
 wc -l *rb.*classification.txt | awk '{print "transcripts assembled: " $1 -1}' >> sqanti_summary.txt # no. of transcripts (-1 for the header line)
+
 grep 'full-splice_match' *rb.*classification.txt | wc -l | awk '{print "FSM:\t" $1}' >> sqanti_summary.txt #for fsm
-grep 'full-splice_match' *rb.*classification.txt | cut -f8 | sort | uniq | wc -l | awk '{print "Unique FSM:\t" $1}' >> sqanti_summary.txt #for unique fsm
-grep 'full-splice_match' *rb.*classification.txt | grep -e 'reference_match' -e 'mono-exon' | wc -l | awk '{print "RM:\t" $1}' >> sqanti_summary.txt #for rm
-grep 'full-splice_match' *rb.*classification.txt | grep -e 'reference_match' -e 'mono-exon' | cut -f8 | sort | uniq | wc -l | awk '{print "Unique RM:\t" $1}' >> sqanti_summary.txt #for unique rm
+grep 'full-splice_match' *rb.*classification.txt | \
+cut -f8 | sort | uniq | wc -l | awk '{print "Unique FSM:\t" $1}' >> sqanti_summary.txt #for unique fsm
+
+grep 'full-splice_match' *rb.*classification.txt | \
+grep -e 'reference_match' -e 'mono-exon' | wc -l | awk '{print "RM:\t" $1}' >> sqanti_summary.txt #for rm
+grep 'full-splice_match' *rb.*classification.txt | \
+grep -e 'reference_match' -e 'mono-exon' | cut -f8 | sort | uniq | wc -l | awk '{print "Unique RM:\t" $1}' >> sqanti_summary.txt #for unique rm
+
 grep 'incomplete-splice_match' *rb.*classification.txt | wc -l | awk '{print "ISM:\t" $1}' >> sqanti_summary.txt #for ism
-grep 'incomplete-splice_match' *rb.*classification.txt | cut -f8 | sort | uniq | wc -l | awk '{print "Unique ISM:\t" $1}' >> sqanti_summary.txt #for unique ism
+grep 'incomplete-splice_match' *rb.*classification.txt | \
+cut -f8 | sort | uniq | wc -l | awk '{print "Unique ISM:\t" $1}' >> sqanti_summary.txt #for unique ism
+
+grep -e 'full-splice_match' -e 'incomplete-splice_match' *rb.*classification.txt | \
+cut -f8 | sort | uniq | wc -l | awk '{print "Unique RM/ISM:\t" $1}' >> sqanti_summary.txt #for unique rm/ism
+
 grep 'novel_in_catalog' *rb.*classification.txt | wc -l | awk '{print "NIC:\t" $1}' >> sqanti_summary.txt # for NIC
 grep 'novel_not_in_catalog' *rb.*classification.txt | wc -l | awk '{print "NNC:\t" $1}' >> sqanti_summary.txt # for NNC
 grep 'antisense' *rb.*classification.txt | wc -l | awk '{print "antisense:\t" $1}'>> sqanti_summary.txt # for antisense
@@ -61,14 +111,25 @@ grep 'genic' *rb.*classification.txt | wc -l | awk '{print "genic:\t" $1}' >> sq
 grep 'intergenic' *rb.*classification.txt | wc -l | awk '{print "intergenic:\t" $1}' >> sqanti_summary.txt # for intergenic
 grep 'fusion' *rb.*classification.txt | wc -l | awk '{print "fusion:\t" $1}' >> sqanti_summary.txt # for fusion
 
+
 echo 'analysis for rattle' >> sqanti_summary.txt
+
 wc -l *rattle.*classification.txt | awk '{print "transcripts assembled: " $1 -1}' >> sqanti_summary.txt # no. of transcripts (-1 for the header line)
+
 grep 'full-splice_match' *rattle.*classification.txt | wc -l | awk '{print "FSM:\t" $1}' >> sqanti_summary.txt #for fsm
-grep 'full-splice_match' *rattle.*classification.txt | cut -f8 | sort | uniq | wc -l | awk '{print "Unique FSM:\t" $1}' >> sqanti_summary.txt #for unique fsm
+grep 'full-splice_match' *rattle.*classification.txt | \
+cut -f8 | sort | uniq | wc -l | awk '{print "Unique FSM:\t" $1}' >> sqanti_summary.txt #for unique fsm
 grep 'full-splice_match' *rattle.*classification.txt | grep -e 'reference_match' -e 'mono-exon' | wc -l | awk '{print "RM:\t" $1}' >> sqanti_summary.txt #for rm
-grep 'full-splice_match' *rattle.*classification.txt | grep -e 'reference_match' -e 'mono-exon' | cut -f8 | sort | uniq | wc -l | awk '{print "Unique RM:\t" $1}' >> sqanti_summary.txt #for unique rm
+grep 'full-splice_match' *rattle.*classification.txt | grep -e 'reference_match' -e 'mono-exon' | \
+cut -f8 | sort | uniq | wc -l | awk '{print "Unique RM:\t" $1}' >> sqanti_summary.txt #for unique rm
+
 grep 'incomplete-splice_match' *rattle.*classification.txt | wc -l | awk '{print "ISM:\t" $1}' >> sqanti_summary.txt #for ism
-grep 'incomplete-splice_match' *rattle.*classification.txt | cut -f8 | sort | uniq | wc -l | awk '{print "Unique ISM:\t" $1}' >> sqanti_summary.txt #for unique ism
+grep 'incomplete-splice_match' *rattle.*classification.txt | \
+cut -f8 | sort | uniq | wc -l | awk '{print "Unique ISM:\t" $1}' >> sqanti_summary.txt #for unique ism
+
+grep -e 'full-splice_match' -e 'incomplete-splice_match' *rattle.*classification.txt | \
+cut -f8 | sort | uniq | wc -l | awk '{print "Unique RM/ISM:\t" $1}' >> sqanti_summary.txt #for unique rm/ism
+
 grep 'novel_in_catalog' *rattle.*classification.txt | wc -l | awk '{print "NIC:\t" $1}' >> sqanti_summary.txt # for NIC
 grep 'novel_not_in_catalog' *rattle.*classification.txt | wc -l | awk '{print "NNC:\t" $1}' >> sqanti_summary.txt # for NNC
 grep 'antisense' *rattle.*classification.txt | wc -l | awk '{print "antisense:\t" $1}'>> sqanti_summary.txt # for antisense
