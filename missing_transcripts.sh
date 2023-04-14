c197c6e6abe16a7d30058e6a19104591f96c14e0
jwyli <76800020+jwyli@users.noreply.github.com>
2023-04-14 16:31:04 +0800

    Update missing_transcripts.sh

--- missing_transcripts.sh
+++ missing_transcripts.sh
@@ -2,12 +2,10 @@
 
 refAnnotation=$1
 
-grep -w transcript $refAnnotation | cut -f9 | cut -f4 -d'"'| sort | uniq >> refGenes.tmp
+grep -oE 'transcript_id "[[:alnum:]]+";' $refAnnotation | cut -f2 -d'"'| sort | uniq >> refGenes.tmp
 
 for assembler in flair stringtie.guided stringtie.free rb rattle
 do
     grep -v 'novel\|associated_transcript' *$assembler*classification.txt| cut -f8 | sort | uniq > $assembler.tmp
     grep -Fxv -f $assembler.tmp refGenes.tmp > $assembler.diff
 done
-
-rm *.tmp
